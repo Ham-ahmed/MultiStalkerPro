d93bfdcd0bd5cebc915acba87d026fb372739e4d
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-03-30 15:48:54 +0200

    Add files via upload

--- Portal_multistalker.sh
+++ Portal_multistalker.sh
@@ -1,26 +1,66 @@
 #!/bin/sh
- # 
-echo ""
+#
 cd /tmp
-curl  -k -Lbk -m 55532 -m 555104 "https://raw.githubusercontent.com/Ham-ahmed/MultiStalkerPro/main/New-20_Portals.tar.gz" > /tmp/New-20_Portals.tar.gz
-sleep 1
-echo "installing ...."
-cd /tmp
+set -e 
+ wget "https://raw.githubusercontent.com/Ham-ahmed/MultiStalkerPro/main/New-20_Portals.tar.gz"
+wait
 tar -xzf New-20_Portals.tar.gz  -C /
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-echo ""
-sleep 1
-cd
+wait
+cd ..
+set +e
 rm -f /tmp/New-20_Portals.tar.gz
-echo "OK"
-echo " UPLOADED BY Hamdy Ahmed
+sleep 2;
+echo "" 
+echo "" 
+echo "**************************************************************************************************"
+echo "#  INSTALLED SUCCESSFULLY #"
+echo "*      888888  888888    .d88b.    88888     88    88   .d88b.  *"
+echo "*      88  88  88  88   88    88   88   88   88    88  88    88 *"
+echo "*      88    88    88  88      88  88    88  88oooo88  88oooo88 *"
+echo "*      88          88   88    88   88    88  88    88  88    88 *"
+echo "*      88          88   .d88b.     888888    88    88  88    88 *"
+echo "*          EDITE - Hamdy Ahmed     Novaler 4k Egypt *"
+echo "*           888888  88    88  oo  8888     88  8888 *"
+echo "*           88      88  88    88  88 88    88  88   *"
+echo "*           88888   8888      88  88  88   88  8888 *"
+echo "*              88   88  88    88  88    88 88    88 *"
+echo "*           88888   88   88   88  88     8888  8888 *"
+echo "*                Enigma2 restart is required           *"
+echo "*************************************************************************************************"
+echo "   UPLOADED BY  >>>>   HAMDY_AHMED "
 sleep 4;
-echo ""
-echo ""
-exit
+	echo '========================================================================================='
+###########################################                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo "*************************************************************************************************"
+wait
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
